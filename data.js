5a65ce930bfdd5ab5c6f6a9260c6f426c75ebaaf
UrbanoTrejoOrlando <orlandourbanotrejo@gmail.com>
2025-02-15 12:39:55 -0600

    Archivo data.js

---
+++ data.js
@@ -0,0 +1,13 @@
+module.exports.users = [
+    {
+        id:1,
+        username: "Orlando",
+        password: "12345"
+    },
+    {
+        id:2,
+        username: "Rojo",
+        password: "54321"
+    }
+
+]
